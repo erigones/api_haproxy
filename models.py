81bbe00810e944ca84596a15be0e04e2f2755105
Dusan Matejka <matejkadusan32@gmail.com>
2015-02-08 23:25:03 +0100

    more verbosity & enhanced model for HaProxy configuration

--- models.py
+++ models.py
@@ -1,26 +1,39 @@
 from django.db import models
+from hashlib import md5
 import json
 import base64
 
 
 class Base64JsonField(models.TextField):
     """
-    Base64JsonField is custom field purposed to store json data in base64 format to text column.
+    Base64JsonField is custom field purposed to store json data in base64 format to text column
+    in relational databases.
     """
     __metaclass__ = models.SubfieldBase
 
     def get_prep_value(self, value):
+        """
+        Encodes json data into base64 when preparing data for write to a database.
+        :param value: data to be serialized first to json, then to base64
+        :return: base64 encoded data
+        """
         if value is not None:
             return 'base64:' + base64.encodestring(json.dumps(value))
 
     def to_python(self, value):
-        # This method is called when data are gathered from the database as well as when the class is instantiated
-        # thus there is need to prepend a string with a prefix and check it to prevent decoding common string
+        """
+        Decodes base64 json serialized data.
+        Method is called when data are gathered from the database as well as when the class is instantiated
+        thus there is need to prepend a string with a prefix and check it to prevent decoding common string.
+        :param value: serialized data from database
+        :return: json data
+        """
         if value is not None and isinstance(value, basestring):
             value = str(value)
             if value.startswith('base64:'):
                 value = value.split(':')[1]
                 return json.loads(base64.decodestring(value))
+            # TODO elif for situation where value is dict
             else:
                 return value
 
@@ -30,3 +43,13 @@ class HaProxyConfigModel(models.Model):
     section = models.CharField(max_length=100)
     meta = Base64JsonField()
     configuration = Base64JsonField()
+
+    def save(self, *args, **kwargs):
+        """
+        Enhances default models.Model.save method to store a configuration checksum for later processing
+        like writes or check of integrity during saves.
+        """
+        checksum = md5()
+        checksum.update(self.configuration)
+        self.checksum = checksum.hexdigest()
+        super(HaProxyConfigModel, self).save(*args, **kwargs)
