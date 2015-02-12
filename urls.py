60afb5e510c5c9a0f5c7212eb9cda19e3e873d7b
Dusan Matejka <matejkadusan32@gmail.com>
2015-02-12 13:49:20 +0000

    complete rework of views for HaProxy configuration

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^test/$', views.TestView.as_view()),
-    url(r'^haproxy/$', views.HaProxyTestView.as_view())
+    url(r'^section/$', views.HaProxyTestView.as_view()),
+    url(r'^section/(?P<pk>\d+)/$', views.HaProxyTestView.as_view())
 ]
--- views.py
+++ views.py
@@ -2,28 +2,37 @@ from rest_framework.views import APIView
 from models import HaProxyConfigModel
 from serializers import HaProxyConfigModelSerializer
 from rest_framework.response import Response
-from api_core.exceptions import InvalidRequestException
-
-
-class TestView(APIView):
-
-    def get(self, request, format=None):
-        return Response({'Result': 'Hello tester!'})
+from api_core.exceptions import InvalidRequestException, DoesNotExistException, DuplicateEntryException
+from django.db import IntegrityError
 
 
 class HaProxyTestView(APIView):
 
-    def get(self, request, format=None):
-        configs = HaProxyConfigModel.objects.all()
-        serializer = HaProxyConfigModelSerializer(configs, many=True)
+    def get(self, request, pk=None, format=None):
+        if pk is not None:
+            try:
+                config = HaProxyConfigModel.objects.get(pk=pk)
+                serializer = HaProxyConfigModelSerializer(config)
+            except HaProxyConfigModel.DoesNotExist:
+                raise DoesNotExistException()
+        else:
+            config = HaProxyConfigModel.objects.all()
+            serializer = HaProxyConfigModelSerializer(config, many=True)
         return Response(serializer.data)
 
     def post(self, request, format=None):
         section = request.DATA.get('section', None)
+        section_name = request.DATA.get('section_name', None)
         configuration = request.DATA.get('configuration', None)
 
-        if not section or not configuration:
+        if not all([x is not None for x in [section, section_name, configuration]]):
             raise InvalidRequestException()
+        else:
+            try:
+                config = HaProxyConfigModel(section=section, section_name=section_name, configuration=configuration)
+                config.save()
+            except IntegrityError:
+                raise DuplicateEntryException()
 
         return Response({'status': 'ok'})
 
