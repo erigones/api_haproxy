dd328ac0b9a841bc36286aff0916ad94228ef511
Dusan Matejka <matejkadusan32@gmail.com>
2015-02-12 10:57:00 +0000

    enhanced HaProxyConfigModel to generate metadata when entry is prepared to be saved to database

--- models.py
+++ models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from hashlib import md5
 from fields import Base64JsonField
+from django.utils import timezone
 
 
 class HaProxyConfigModel(models.Model):
+    """
+    Model storing serialized configuration of HaProxy divided per section in Base64 format. E.g. frontend, backend.
+    """
     checksum = models.CharField(max_length=32)
     section = models.CharField(max_length=100)
     section_name = models.CharField(max_length=100)
@@ -18,4 +22,15 @@ class HaProxyConfigModel(models.Model):
         checksum = md5()
         checksum.update(self.configuration)
         self.checksum = checksum.hexdigest()
+        self.meta = self.generate_meta()
         super(HaProxyConfigModel, self).save(*args, **kwargs)
+
+    def generate_meta(self):
+        """
+        Method generates metadata for every submitted entry to database.
+        :return: list of metadata for entry
+        """
+        meta = {
+            'create_time': timezone.now(),
+        }
+        return meta
