59b4e95d9410bd3921493bb3cadf22326033d3ce
Dusan Matejka <matejkadusan32@gmail.com>
2015-02-22 08:53:15 +0100

    refactored views to contain comments and added check of json input data

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^section/$', views.HaProxyTestView.as_view()),
-    url(r'^section/(?P<pk>\d+)/$', views.HaProxyTestView.as_view())
+    url(r'^section/$', views.HaProxyConfigBuildView.as_view()),
+    url(r'^section/(?P<checksum>\w+)/$', views.HaProxyConfigBuildView.as_view())
 ]
--- views.py
+++ views.py
@@ -4,14 +4,25 @@ from serializers import HaProxyConfigModelSerializer
 from rest_framework.response import Response
 from api_core.exceptions import InvalidRequestException, DoesNotExistException, DuplicateEntryException
 from django.db import IntegrityError
+import json
 
 
-class HaProxyTestView(APIView):
+class HaProxyConfigBuildView(APIView):
+    """
+    API view handling build process of sections in HaProxy configuration.
+    """
 
-    def get(self, request, pk=None, format=None):
-        if pk is not None:
+    def get(self, request, checksum=None):
+        """
+        Method responding to GET request lists specific configuration section stored in a database whenever
+        unique checksum string is provided. Otherwise list of all configuration sections is returned in response.
+        :param request: request data
+        :param checksum: unique identifier of configuration sections
+        :return: rest_framework.response.Response containing serialized data
+        """
+        if checksum is not None:
             try:
-                config = HaProxyConfigModel.objects.get(pk=pk)
+                config = HaProxyConfigModel.objects.get(checksum=checksum)
                 serializer = HaProxyConfigModelSerializer(config)
             except HaProxyConfigModel.DoesNotExist:
                 raise DoesNotExistException()
@@ -20,7 +31,13 @@ class HaProxyTestView(APIView):
             serializer = HaProxyConfigModelSerializer(config, many=True)
         return Response(serializer.data)
 
-    def post(self, request, format=None):
+    def post(self, request):
+        """
+        Method responding to POST request creates new configuration section, validates input and
+        stores it in a database.
+        :param request: request data
+        :return: rest_framework.response.Response containing serialized data
+        """
         section = request.DATA.get('section', None)
         section_name = request.DATA.get('section_name', None)
         configuration = request.DATA.get('configuration', None)
@@ -30,10 +47,13 @@ class HaProxyTestView(APIView):
             raise InvalidRequestException()
         else:
             try:
+                json.loads(configuration)  # Should raise ValueError if configuration data are invalid
                 config = HaProxyConfigModel(section=section, section_name=section_name, configuration=configuration)
                 config.save()
             except IntegrityError:
                 raise DuplicateEntryException()
+            except ValueError:
+                raise InvalidRequestException()
 
         return Response({'status': 'ok'})
 
